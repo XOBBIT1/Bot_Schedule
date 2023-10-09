32b32fc347c59a326152660a77aa9147cc81aa78
Денис Гришанков <XOBBIT1>
2023-10-09 18:38:44 +0300

    fixing error

--- app/core/servises/serves_training.py
+++ app/core/servises/serves_training.py
@@ -1,33 +1,55 @@
+import logging
+
 from app.core.servises.serves_user import db_session
 from app.settings.bd.models import Trainings
 
 
 async def create_training(training_time: str, training_day: str):
-    db_session.add(Trainings(
-        training_day=training_day,
-        training_time=training_time,
-    ))
-    db_session.commit()
-    db_session.close()
+    try:
+        db_session.add(Trainings(
+            training_day=training_day,
+            training_time=training_time,
+        ))
+        db_session.commit()
+        db_session.close()
+    except Exception as e:
+        logging.info(e)
+        db_session.rollback()
 
 
 async def get_all_trainings():
-    ids = db_session.query(Trainings).all()
-    return ids
+    try:
+        ids = db_session.query(Trainings).all()
+        return ids
+    except Exception as e:
+        logging.info(e)
+        db_session.rollback()
 
 
 async def get_all_not_booked_trainings():
-    ids = db_session.query(Trainings).filter_by(booked=False).all()
-    return ids
+    try:
+        ids = db_session.query(Trainings).filter_by(booked=False).all()
+        return ids
+    except Exception as e:
+        logging.info(e)
+        db_session.rollback()
 
 
 async def delete_training_by_id(training_id):
-    element_to_delete = db_session.query(Trainings).filter_by(id=training_id).first()
-    db_session.delete(element_to_delete)
-    db_session.commit()
-    db_session.close()
+    try:
+        element_to_delete = db_session.query(Trainings).filter_by(id=training_id).first()
+        db_session.delete(element_to_delete)
+        db_session.commit()
+        db_session.close()
+    except Exception as e:
+        logging.info(e)
+        db_session.rollback()
 
 
 async def checking_users_with_trainings():
-    ids = db_session.query(Trainings).filter_by(booked=True).all()
-    return ids
+    try:
+        ids = db_session.query(Trainings).filter_by(booked=True).all()
+        return ids
+    except Exception as e:
+        logging.info(e)
+        db_session.rollback()
--- app/core/servises/serves_user.py
+++ app/core/servises/serves_user.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 
 from app.settings.bd.models import Trainings, Users
 
@@ -8,48 +9,74 @@ db_session = create_dbsession()
 
 
 async def check_user(message):
-    return db_session.query(Users).filter_by(chat_id=message.chat.id).count()
+    with db_session as session:
+        try:
+            return session.query(Users).filter_by(chat_id=message.chat.id).count()
+        except Exception as e:
+            logging.info(e)
+            session.rollback()
 
 
 async def create_user(message, user_name: str):
-    db_session.add(Users(
-        user_name=user_name,
-        chat_id=message.chat.id,
-        created_at=datetime.datetime.now(),
-        last_appear=datetime.datetime.now()
-    ))
-    db_session.commit()
-    db_session.close()
+    try:
+        db_session.add(Users(
+            user_name=user_name,
+            chat_id=message.chat.id,
+            created_at=datetime.datetime.now(),
+            last_appear=datetime.datetime.now()
+        ))
+        db_session.commit()
+        db_session.close()
+    except Exception as e:
+        logging.info(e)
+        db_session.rollback()
 
 
 async def get_user_name(message):
-    chat_ids = db_session.query(Users).filter_by(chat_id=message.chat.id)
-    for chat_id in chat_ids:
-        return chat_id.user_name
+    with db_session as session:
+        try:
+            chat_ids = session.query(Users).filter_by(chat_id=message.chat.id)
+            for chat_id in chat_ids:
+                return chat_id.user_name
+        except Exception as e:
+            logging.info(e)
+            db_session.rollback()
 
 
 async def sign_up_user_for_training(message, training_id: str):
-    training = db_session.query(Trainings).filter_by(id=training_id).first()
+    try:
+        training = db_session.query(Trainings).filter_by(id=training_id).first()
 
-    if training:
-        user = db_session.query(Users).filter_by(chat_id=message.chat.id).first()
-        if user:
-            training.user = user
-            training.booked = True
-            db_session.commit()
+        if training:
+            user = db_session.query(Users).filter_by(chat_id=message.chat.id).first()
+            if user:
+                training.user = user
+                training.booked = True
+                db_session.commit()
+    except Exception as e:
+        logging.info(e)
+        db_session.rollback()
 
 
 async def cancel_training(message, training_id: str):
-    training = db_session.query(Trainings).filter_by(id=training_id).first()
+    try:
+        training = db_session.query(Trainings).filter_by(id=training_id).first()
 
-    if training:
-        user = db_session.query(Users).filter_by(chat_id=message.chat.id).first()
-        if user:
-            training.user = None
-            training.booked = False
-            db_session.commit()
+        if training:
+            user = db_session.query(Users).filter_by(chat_id=message.chat.id).first()
+            if user:
+                training.user = None
+                training.booked = False
+                db_session.commit()
+    except Exception as e:
+        logging.info(e)
+        db_session.rollback()
 
 
 async def user_trainings(message):
-    user = db_session.query(Users).filter_by(chat_id=message.chat.id).first()
-    return user.trainings
+    try:
+        user = db_session.query(Users).filter_by(chat_id=message.chat.id).first()
+        return user.trainings
+    except Exception as e:
+        logging.info(e)
+        db_session.rollback()
